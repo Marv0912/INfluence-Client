1c8fc9047b2806f3bb0d05cdfb84d5c8cf0a57e5
Marv0912 <marvin.fajardo.s@icloud.com>
2024-01-16 18:31:54 -0400

    create routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import Navbar from './components/Navbar'
 import { useContext } from 'react'
 import { AuthContext } from './context/auth.context'
-import { Route, Routes, Outlet, Navigate } from 'react-router-dom'
+import { Route, Routes  } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home'
-import Login from './pages/Login'
+import Home from './pages/Home'
 import Signup from './pages/Signup'
-import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Search from './pages/Search' // search based on niche, location and followers
+import Results from './pages/Results' // Results based on search
+import InfluencerDetails from './pages/InfluencerDetails' // As a company when clicking on searched result, you can see the influencer details
+import Database from './pages/Database' // If company has saved users, they will be displayed in a table here
+import Profile from './pages/Profile' // Logged In user details
+// TODO: Create an edit profile page || when clicking edit on Profile, change it to form type
 
 function App() {
   const { getToken } = useContext(AuthContext)
-  const isLoggedIn = () => {
-    return getToken() ? <Outlet /> : <Navigate to='/login' />
 
-}
-
-  const isLoggedOut = () => {
-    return !getToken() ? <Outlet /> : <Navigate to='/'>
-  }
+  // const isLoggedOut = () => {
+  //     if (!getToken()) {
+  //       return <Routes>
+  //         <Route path='/login' element={<Login />} />
+  //         <Route path='/signup' element={<Signup />} />
+  //       </Routes>;
+  //     } else {
+  //       return <Navigate to='/' />;
+  //     }
+  //   };
   return (
     <>
-     <Navbar></Navbar>
-     <Routes>
-      <Route path='/' element={< Home/>}></Route>
-      <Route element={<isLoggedOut/>} >
-        <Route path='/login' element={< Login/>}></Route>
-        <Route path='/signup' element={< Signup/>}></Route>
-      </Route>
-
-     </Routes>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={< Home />}></Route>
+        {/* TODO: Protect login & signup routes if user already logged in */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/results' element={<Results />}></Route>
+        <Route path='/influencer/:id' element={<InfluencerDetails />}></Route>
+        <Route path='/database' element={<Database />}></Route>
+        <Route path='/user/:id' element={<Profile />}></Route>
+        {/* Create Dropdown menu on */}
+        {/* {isLoggedOut()} */}
+      </Routes>
     </>
   )
 }
